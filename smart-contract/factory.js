3096ea8d444c040018751f3a3b49d5798436a24e
uselessengineer10 <patutamboli@gmail.com>
2022-01-28 11:26:31 +0530

    contract modified

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,8 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
+  "0x032e94430c1EA762F12a8392eeEB000d64EB3A08"
+  //"0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
 );
 
 export default instance;
--- smart-contract/web3.js
+++ smart-contract/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // we are on the server *OR* meta mask is not running
   // creating our own provider
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/08ac79d88b5d4aea961ca36af7ea6ee7"
+    "https://rinkeby.infura.io/v3/236f9372e6d24eea97a2db0409233bb4"
   );
 
   web3 = new Web3(provider);
